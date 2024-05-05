93f19cacc94381e29b93ad873c0c13a761932cc4
developerYeasin <yea13sin@gmail.com>
2024-05-05 16:04:13 +0600

    first commit

---
+++ 01_demo_project/src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h2 className="text-[100px] text-red-500 font-bold underline  " >this is home pge</h2>
+        </div>
+    );
+};
+
+export default Home;
